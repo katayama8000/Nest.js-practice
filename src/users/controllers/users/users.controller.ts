d964645e958b14b3b57c6811dcca4e594fb369d7
katayama <tattu.0310@gmail.com>
2022-11-15 21:42:34 +0900

    feat: exception

--- src/users/controllers/users/users.controller.ts
+++ src/users/controllers/users/users.controller.ts
@@ -6,8 +6,12 @@ import {
   HttpStatus,
   Inject,
   Param,
+  ParseIntPipe,
+  UseFilters,
   UseInterceptors,
 } from '@nestjs/common';
+import { UserNotFoundException } from 'src/users/exceptions/UserNotFound.exception';
+import { HttpExceptionFilter } from 'src/users/filters/HttpException.filter';
 import { SerializedUser } from 'src/users/types';
 
 @Controller('users')
@@ -20,10 +24,26 @@ export class UsersController {
   }
 
   @UseInterceptors(ClassSerializerInterceptor)
-  @Get('/:username')
+  @Get('username/:username')
   getUsersByUsername(@Param('username') username: string) {
     const user = this.usersService.getUsersByUsername(username);
     if (user) return new SerializedUser(user);
     else throw new HttpException('User not found', HttpStatus.BAD_REQUEST);
+  }
+
+  @UseInterceptors(ClassSerializerInterceptor)
+  @UseFilters(HttpExceptionFilter)
+  @Get('id/:id')
+  getUserById(@Param('id', ParseIntPipe) id: number) {
+    console.log(typeof id);
+    const user = this.usersService.getUserById(id);
+    console.log(user);
+    if (user) {
+      console.log('user: ', user);
+      return new SerializedUser(user);
+    } else {
+      console.log('user not found');
+      throw new UserNotFoundException('User not found', 400);
+    }
   }
 }
--- src/users/services/users/users.service.ts
+++ src/users/services/users/users.service.ts
@@ -4,10 +4,10 @@ import { SerializedUser, User } from '../../types';
 @Injectable()
 export class UsersService {
   private users: User[] = [
-    { username: 'denny', password: 'denny' },
-    { username: 'zedd', password: 'zedd' },
-    { username: 'qin', password: 'qin' },
-    { username: 'cat', password: 'cat' },
+    { id: 1, username: 'denny', password: 'denny' },
+    { id: 2, username: 'zedd', password: 'zedd' },
+    { id: 3, username: 'qin', password: 'qin' },
+    { id: 4, username: 'cat', password: 'cat' },
   ];
 
   getUsers() {
@@ -16,5 +16,9 @@ export class UsersService {
 
   getUsersByUsername(username: string) {
     return this.users.find((user) => user.username === username);
+  }
+
+  getUserById(id: number): User {
+    return this.users.find((user) => user.id === id);
   }
 }
