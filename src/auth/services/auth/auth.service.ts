30150beedcda55fe392fea793d9d446ca56758e3
katayama <tattu.0310@gmail.com>
2022-11-17 17:44:20 +0900

    feat: hash

--- src/auth/services/auth/auth.service.ts
+++ src/auth/services/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/services/users/users.service';
+import { comparePassword } from 'src/utils/bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -11,9 +12,15 @@ export class AuthService {
     console.log('username : ', username, 'password : ', password);
     console.log('Inside AuthService.validateUser()');
     const userDB = await this.userService.findUserByUsername(username);
-    if (userDB && userDB.password === password) {
-      console.log('成功: ', userDB);
-      return userDB;
+    if (userDB) {
+      const isMatched = comparePassword(password, userDB.password);
+      if (isMatched) {
+        console.log('成功: ', userDB);
+        return userDB;
+      } else {
+        console.log('パスワードが違います');
+        return null;
+      }
     } else {
       console.log('失敗');
       return null;
--- src/utils/bcrypt.ts
+++ src/utils/bcrypt.ts
@@ -4,3 +4,7 @@ export function encodePassword(rawPassword: string) {
   const salt = bcrypt.genSaltSync();
   return bcrypt.hashSync(rawPassword, salt);
 }
+
+export function comparePassword(rawPassword: string, hash: string) {
+  return bcrypt.compareSync(rawPassword, hash);
+}
